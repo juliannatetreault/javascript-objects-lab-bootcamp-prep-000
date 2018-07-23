514c3799c2efdc56570a455964f55dc99e647dad
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-23 05:28:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = new Object({});
+
+var obj = { prop: 1 }
+
+var newObj =  Object.assign({}, obj)
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, obj, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object [key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete newObj.key;
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object
+}
