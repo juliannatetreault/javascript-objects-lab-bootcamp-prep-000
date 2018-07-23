9d25d5e102c55afa30d625bc42e51a36969d0605
Julianna Tetreault <juliannatetreault@icloud.com>
2018-07-23 05:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObj =  Object.assign({}, obj)
   delete newObj.key;
   return obj
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object
-}
+
